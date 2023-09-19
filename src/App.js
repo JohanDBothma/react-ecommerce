67c1b94785f252d9e19a79ab7553b5b6a84e2012
Johan <johandanielbothma@gmail.com>
2023-09-19 15:09:05 +0200

    Setup basic navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React  from 'react';
-import { Routes, Route, Outlet } from 'react-router-dom'
-import Home from "./routes/home/home.component";
+import { Routes, Route } from 'react-router-dom'
 
-const Navigation = () => {
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+const Shop = () => {
     return (
         <div>
-            <div>
-                <h1>I am nav</h1>
-            </div>
+            <h1>Shop page</h1>
         </div>
     )
 }
@@ -17,6 +18,8 @@ const App = () => {
         <Routes>
             <Route path='/' element={<Navigation />}>
                 <Route index element={<Home/>} />
+                <Route path="/shop" element={<Shop/>} />
+                <Route path="sign-in" element={<SignIn />} />
             </Route>
         </Routes>
     );
